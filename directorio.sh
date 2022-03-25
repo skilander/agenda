8b56c9f3d40f9ec2324d2b1909d656b83a7f4930
skilander <102385011+skilander@users.noreply.github.com>
2022-03-25 13:33:08 -0600

    la agenda

---
+++ directorio.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "ingresar datos personales"
+read n 
+echo "email:"
+read n
+echo "signo zodiacal:"
+read n
+echo "edad:"
+read n
+echo "nombre completo:"
+read n
+echo "muchas gracias"
+read n
